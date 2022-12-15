ad419d0602a91c703639b9ea8b21dc45238bd01d
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-12-15 11:43:55 +0100

    Add fixtures

--- src/conftest.py
+++ src/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from network_parser import NetworkParser
+from node import Node
 
 
 @pytest.fixture
@@ -7,3 +8,17 @@ def sample_network_parser():
     test_network_parser = NetworkParser(
         "Test Network", "../fixtures/lines.json", "../fixtures/stops.json")
     return test_network_parser
+
+@pytest.fixture
+def node_samples():
+    id_node = 1
+    name = "station01"
+    id_lines = [1, 2]
+    lat, lon = [48.861675, 2.346786]
+    node_1 = Node(id_node, name, id_lines, [lon, lat])
+    id_node = 2
+    name = "station02"
+    id_lines = [1]
+    lat, lon = [48.854921, 2.347367]
+    node_2 = Node(id_node, name, id_lines, [lon, lat])
+    return (node_1, node_2)
